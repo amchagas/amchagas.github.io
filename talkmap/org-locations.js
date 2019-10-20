f95beb03e64d6199a8f6a16552e4626399c8040a
amchagas <andremaia.chagas@gmail.com>
2019-10-20 14:44:42 +0100

    update talk map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,67 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Paris, France",
+    48.8566101,
+    2.3514992
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Tuebingen, Germany",
+    48.5236164,
+    9.0535531
+  ],
+  [
+    "Durban, South Africa",
+    -29.861825,
+    31.009909
+  ],
+  [
+    "Santiago, Chile",
+    -33.4377968,
+    -70.6504451
+  ],
+  [
+    "Delft, Netherlands",
+    51.9994572,
+    4.362724538544
+  ],
+  [
+    "Cape Town, South Africa",
+    -33.928992,
+    18.417396
+  ],
+  [
+    "Sao Paulo, Brazil",
+    -23.5506507,
+    -46.6333824
+  ],
+  [
+    "New York, New York",
+    40.7127281,
+    -74.0060152
+  ],
+  [
+    "Brighton, UK",
+    50.8220399,
+    -0.1374061
+  ],
+  [
+    "Mendoza, Argentina",
+    -32.8897294,
+    -68.8442956
+  ],
+  [
+    "Mafra, Portugal",
+    38.9369782,
+    -9.3282374
+  ],
+  [
+    "Khartoum, Sudan",
+    15.593325,
+    32.53565
   ],
   [
     "San Francisco, California",
     37.7792808,
-    -122.4192362
-  ],
-  [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    -122.4192363
   ]
 ];
